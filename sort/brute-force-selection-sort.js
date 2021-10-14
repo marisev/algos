2c3f5e55c5d6c8dd131d4f0d20cafc846e02d706
marinasev <marina.v.sevostyanova@gmail.com>
2021-10-14 13:07:09 -0400

    selection sort

---
+++ sort/brute-force-selection-sort.js
@@ -0,0 +1,33 @@
+const testsets = [
+  {
+    initial: [3, 2, 5, 4, 10, 7],
+    expected: [2, 3, 4, 5, 7, 10]
+  },
+  {
+    initial: [0],
+    expected: [0]
+  },
+];
+
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = arr[i];
+    let minInd = i;
+
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < min) {
+        min = arr[j];
+        minInd = j;
+      }
+    }
+    if (minVal < arr[i]) {
+      [arr[i], arr[minInd]] = [arr[minInd], arr[i]];
+    }
+  }
+  console.log(arr);
+}
+
+
+selectionSort(testsets[0].initial);
+selectionSort(testsets[1].initial);
